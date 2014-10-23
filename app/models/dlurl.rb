b7a5c36463ce99ac81cafefd5b4f7e56358bd3db
iggyigner <jonathan.igner@gmail.com>
2014-10-23 15:50:47 -0700

    ensure deliver_url is converting 'artist' to string with to_s

--- app/models/dlurl.rb
+++ app/models/dlurl.rb
@@ -1,6 +1,6 @@
 class Dlurl < ActiveRecord::Base
 	def self.deliver_url(response, artist)
-		download_url = Dlurl.where(:artist => artist, :used => false).first
+		download_url = Dlurl.where(:artist => artist.to_s, :used => false).first
 		download_url.used = true
 		download_url.save
 		data = {"stamp_serial" => response["stamp"]["serial"], "download_url" => download_url.address}
